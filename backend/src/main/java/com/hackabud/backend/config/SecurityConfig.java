900e3792014b811a753980c7ffc95e07fbe11c62
Witherpizza <79527145+Witherpizza@users.noreply.github.com>
2024-09-28 23:03:43 -0400

    refactor: cleaned up security config

--- backend/src/main/java/com/hackabud/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/hackabud/backend/config/SecurityConfig.java
@@ -6,22 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-	private static final RegexRequestMatcher SECURITY_PATTERN = new RegexRequestMatcher("^(?!.*\\/sec\\/).*$", null, true);
 
     @Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,11 +18,9 @@ public class SecurityConfig {
 			.csrf(AbstractHttpConfigurer::disable)
 			.cors(c -> c.configure(http))
 			.authorizeHttpRequests(r -> r
-				.requestMatchers(SECURITY_PATTERN)
-				.permitAll()
-				.anyRequest()
-				.authenticated())
-			.oauth2Login(Customizer.withDefaults());
+				.requestMatchers("/api/sec/**").authenticated() // Allow access if authenticated
+				.requestMatchers("/**").permitAll())
+			.oauth2ResourceServer(Customizer.withDefaults());
 		return http.build();
 	}
     
